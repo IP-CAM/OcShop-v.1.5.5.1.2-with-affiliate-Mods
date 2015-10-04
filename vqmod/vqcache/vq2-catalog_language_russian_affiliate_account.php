d7224b8729a9ca4eb18a52671bfbfa4f9811ed7a
Alexey <freerun-2012@yandex.ru>
2015-10-04 17:12:29 +0300

    ver1(work mmm)

---
+++ vqmod/vqcache/vq2-catalog_language_russian_affiliate_account.php
@@ -0,0 +1,22 @@
+<?php
+// Heading 
+$_['heading_title']        = 'Партнерcкий раздел';
+
+// Text
+
+			
+$_['text_my_statisticsmyaffiliate']    = 'Статистика моих партнеров';
+$_['text_statisticsmyaffiliate'] = 'Статистика моих партнеров';
+$_['statisticsmyaffiliate'] = 'Посмотреть статистику моих партнеров';
+			
+			
+$_['text_account']         = 'Кабинет Партнера';
+$_['text_my_account']      = 'Моя учетная запись';
+$_['text_my_tracking']     = 'Мои рефералы';
+$_['text_my_transactions'] = 'История выплат';
+$_['text_edit']            = 'Редактировать учетную запись';
+$_['text_password']        = 'Изменить пароль';
+$_['text_payment']         = 'Изменить платежные реквизиты';
+$_['text_tracking']        = 'Реферальный код';
+$_['text_transaction']     = 'Посмотреть историю выплат';
+?>
