4a0abe8313779b37ca89e3c99b12cea543bc7cb4
Alexey <freerun-2012@yandex.ru>
2015-10-04 16:36:42 +0300

    original

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop-oc2.loc/admin/');
+define('HTTP_CATALOG', 'http://shop-oc2.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop-oc2.loc/admin/');
+define('HTTPS_CATALOG', 'http://shop-oc2.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\OpenServer\domains\shop-oc2.loc/admin/');
+define('DIR_SYSTEM', 'E:\OpenServer\domains\shop-oc2.loc/system/');
+define('DIR_DATABASE', 'E:\OpenServer\domains\shop-oc2.loc/system/database/');
+define('DIR_LANGUAGE', 'E:\OpenServer\domains\shop-oc2.loc/admin/language/');
+define('DIR_TEMPLATE', 'E:\OpenServer\domains\shop-oc2.loc/admin/view/template/');
+define('DIR_CONFIG', 'E:\OpenServer\domains\shop-oc2.loc/system/config/');
+define('DIR_IMAGE', 'E:\OpenServer\domains\shop-oc2.loc/image/');
+define('DIR_CACHE', 'E:\OpenServer\domains\shop-oc2.loc/system/cache/');
+define('DIR_DOWNLOAD', 'E:\OpenServer\domains\shop-oc2.loc/download/');
+define('DIR_LOGS', 'E:\OpenServer\domains\shop-oc2.loc/system/logs/');
+define('DIR_CATALOG', 'E:\OpenServer\domains\shop-oc2.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mysql');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'shop-oc2');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop-oc2.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop-oc2.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\OpenServer\domains\shop-oc2.loc/catalog/');
+define('DIR_SYSTEM', 'E:\OpenServer\domains\shop-oc2.loc/system/');
+define('DIR_DATABASE', 'E:\OpenServer\domains\shop-oc2.loc/system/database/');
+define('DIR_LANGUAGE', 'E:\OpenServer\domains\shop-oc2.loc/catalog/language/');
+define('DIR_TEMPLATE', 'E:\OpenServer\domains\shop-oc2.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\OpenServer\domains\shop-oc2.loc/system/config/');
+define('DIR_IMAGE', 'E:\OpenServer\domains\shop-oc2.loc/image/');
+define('DIR_CACHE', 'E:\OpenServer\domains\shop-oc2.loc/system/cache/');
+define('DIR_DOWNLOAD', 'E:\OpenServer\domains\shop-oc2.loc/download/');
+define('DIR_LOGS', 'E:\OpenServer\domains\shop-oc2.loc/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mysql');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'shop-oc2');
+define('DB_PREFIX', 'oc_');
+?>
